93e7ad3b1ebd6817adeac078f1736297373d643f
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2025-12-30 22:14:48 -0300

    feat: add expense tracking logic and executeExpense function

--- contracts/SchoolFund.sol
+++ contracts/SchoolFund.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract SchoolFund {
+
+    struct Expense {
+        uint256 amount;
+        string description;
+        address recipient;
+    }
     
     address public treasurer;
     uint256 public totalFund;
+
+    Expense[] public expenseHistory;
 
     // Mapping to track how much each parent has contributed
     mapping(address => uint256) public contributions;
@@ -20,5 +28,22 @@ contract SchoolFund {
         
         contributions[msg.sender] += msg.value;
         totalFund += msg.value;
+    }
+
+    function executeExpense(uint256 _amount, string memory _description, address payable _recipient) public {
+        require(msg.sender == treasurer, "Only the treasurer can execute expenses");
+        require(_amount <= address(this).balance, "Insufficient funds in treasury");
+
+        // Transfer funds to the vendor/provider
+        _recipient.transfer(_amount);
+
+        // Record the expense for public auditing
+        expenseHistory.push(Expense({
+            amount: _amount,
+            description: _description,
+            recipient: _recipient
+        }));
+        
+        totalFund -= _amount;
     }
 }
